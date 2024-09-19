944d50cc95426c5131905d1a7d5c22c9784f669d
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-09-19 17:55:21 +0900

    페이지5 스킬 이미지 불러오기

--- project/src/components/main/page5/Page5.jsx
+++ project/src/components/main/page5/Page5.jsx
@@ -1,10 +1,49 @@
 import React from "react";
 import styled from "styled-components";
+import htmlSrc from "../../../assets/skill/html.png";
+import cssSrc from "../../../assets/skill/css.png";
+import jsSrc from "../../../assets/skill/js.png";
+import tsSrc from "../../../assets/skill/ts.png";
+import reactSrc from "../../../assets/skill/react.png";
+import nextSrc from "../../../assets/skill/next.png";
+import styledSrc from "../../../assets/skill/styled.png";
+import muiSrc from "../../../assets/skill/mui.png";
+import framerSrc from "../../../assets/skill/framer.png";
+import nodeSrc from "../../../assets/skill/node.png";
+import expressSrc from "../../../assets/skill/express.png";
+import firebaseSrc from "../../../assets/skill/firebase.png";
+import supabaseSrc from "../../../assets/skill/supabase.png";
+import notionSrc from "../../../assets/skill/notion.png";
+import figmaSrc from "../../../assets/skill/figma.png";
+import discordSrc from "../../../assets/skill/discord.png";
 
 const Page5 = () => {
+  const imgSrcArr = [
+    htmlSrc,
+    cssSrc,
+    jsSrc,
+    tsSrc,
+    reactSrc,
+    nextSrc,
+    styledSrc,
+    muiSrc,
+    framerSrc,
+    nodeSrc,
+    expressSrc,
+    firebaseSrc,
+    supabaseSrc,
+    notionSrc,
+    figmaSrc,
+    discordSrc,
+  ];
+
   return (
     <>
-      <Container></Container>
+      <Container>
+        {imgSrcArr.map((el) => {
+          return <img src={el} width="50px" height="50px"/>;
+        })}
+      </Container>
     </>
   );
 };
